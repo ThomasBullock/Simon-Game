6970dc7e59e399d8d161e4cc1c52b27c1b4d48e7
Thomas Bullock <motbollox@gmail.com>
2017-03-16 20:32:45 +1100

    Added LED beside strict button and toggleStrict func

--- src/components/Strict.js
+++ src/components/Strict.js
@@ -2,12 +2,26 @@ import React from 'react';
 import '../css/Strict.css';
 
 class Strict extends React.Component {
+	constructor() {
+		super()
+		
+		this.checkStrictMode = this.checkStrictMode.bind(this);
+	}
+	checkStrictMode(){
+		if(this.props.strict) {
+			return "strict-LED-on";
+		} else {
+			return "strict-LED";
+		}
+	}
+	
 	
 	render() {
 		return (
-			<div>
+			<div className="btn-space">
 				<label>STRICT</label>
-				<input className="strict-btn" type="button"/>							
+				<input className="strict-btn" type="button" onClick={this.props.toggleStrict}/>
+				<div className={this.checkStrictMode()}></div>							
 			</div>
 		)
 		
